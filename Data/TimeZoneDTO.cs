a3ff52b7207c7d94e15e0694029a6f2850568f23
capelam <mario.capela@stpierre-bru.be>
2023-08-25 15:00:45 +0200

    Resolved getting countrys locations probleme

--- Data/TimeZoneDTO.cs
+++ Data/TimeZoneDTO.cs
@@ -12,7 +12,7 @@ public class TimeZoneDTO
     public string? a { get; set; }
     public List<string>? c { get; set; }
     public int? d { get; set; }
-    public int e { get; set; }
+    public int? e { get; set; }
     public int? r { get; set; }
     public int? u { get; set; }
 }
